2f970079d5915364cac737c1a3366a95e220949e
Porping <porsteam@hotmail.com>
2022-06-01 21:54:58 +0700

    feat:test

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/my-app/' : '',
+};
+
 module.exports = nextConfig
